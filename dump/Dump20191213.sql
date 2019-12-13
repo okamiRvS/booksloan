e86b95e51bf1098338dbd8aa8a8ee626a8e1dafe
Ivo Junior Bettini <king@Air-di-King.lan>
2019-12-13 15:51:00 +0100

    update dump

---
+++ dump/Dump20191213.sql
@@ -0,0 +1,240 @@
+CREATE DATABASE  IF NOT EXISTS `booksloan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `booksloan`;
+-- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: booksloan
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autore`
+--
+
+DROP TABLE IF EXISTS `autore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `autore` (
+  `id_autore` int(11) NOT NULL,
+  `nome` text,
+  `cognome` text,
+  PRIMARY KEY (`id_autore`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autore`
+--
+
+LOCK TABLES `autore` WRITE;
+/*!40000 ALTER TABLE `autore` DISABLE KEYS */;
+INSERT INTO `autore` VALUES (1,'Andrea','Camilleri'),(2,'Elena','Ferrante'),(3,'J.R.R','Tolkien'),(4,'J.K','Rowling'),(5,'Jane','Austen'),(6,'Donna','Tartt'),(7,'Douglas','Adams');
+/*!40000 ALTER TABLE `autore` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `isbn` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `author` text,
+  PRIMARY KEY (`isbn`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Harry Potter e la pietra di merda','Chuck Norris'),(2,'Cime di Rapa','Lino Banfi ');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `copia`
+--
+
+DROP TABLE IF EXISTS `copia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `copia` (
+  `isbn` bigint(20) NOT NULL,
+  `id` int(11) DEFAULT NULL,
+  `disponibilita` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`isbn`),
+  KEY `fk_copia_libro1_idx` (`id`),
+  CONSTRAINT `fk_copia_libro1` FOREIGN KEY (`id`) REFERENCES `libro` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `copia`
+--
+
+LOCK TABLES `copia` WRITE;
+/*!40000 ALTER TABLE `copia` DISABLE KEYS */;
+INSERT INTO `copia` VALUES (123,101,1),(456,109,0);
+/*!40000 ALTER TABLE `copia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro` (
+  `id` int(11) NOT NULL,
+  `titolo` text,
+  `anno` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro`
+--
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+INSERT INTO `libro` VALUES (101,'Guida galattica per gli autostoppisti',1980),(102,'Il cardellino',2013),(103,'L\'amica geniale',2011),(104,'Quindici giorni con Montalbano',1999),(105,'Orgoglio e pregiudizio',1813),(106,'Lo hobbit',1937),(107,'Emma',1915),(108,'Harry Potter e la pietra filosofale',1997),(109,'Dio di illusioni',1992),(110,'Harry Potter e la camera dei segreti',1998);
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prestito`
+--
+
+DROP TABLE IF EXISTS `prestito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestito` (
+  `data_inizio` date DEFAULT NULL,
+  `data_consegna` date DEFAULT NULL,
+  `n_tessera` int(11) NOT NULL,
+  `isbn` bigint(20) NOT NULL,
+  PRIMARY KEY (`n_tessera`,`isbn`),
+  KEY `n_tessera_idx` (`n_tessera`),
+  KEY `fk_prestito_copia1_idx` (`isbn`),
+  CONSTRAINT `fk_prestito_copia1` FOREIGN KEY (`isbn`) REFERENCES `copia` (`isbn`),
+  CONSTRAINT `n_tessera` FOREIGN KEY (`n_tessera`) REFERENCES `utente` (`n_tessera`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestito`
+--
+
+LOCK TABLES `prestito` WRITE;
+/*!40000 ALTER TABLE `prestito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prestito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scritto`
+--
+
+DROP TABLE IF EXISTS `scritto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `scritto` (
+  `id` int(11) NOT NULL,
+  `id_autore` int(11) NOT NULL,
+  KEY `id_idx` (`id`),
+  KEY `id_autore_idx` (`id_autore`),
+  CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `libro` (`id`),
+  CONSTRAINT `id_autore` FOREIGN KEY (`id_autore`) REFERENCES `autore` (`id_autore`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scritto`
+--
+
+LOCK TABLES `scritto` WRITE;
+/*!40000 ALTER TABLE `scritto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scritto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequel`
+--
+
+DROP TABLE IF EXISTS `sequel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sequel` (
+  `id_1` int(11) NOT NULL,
+  `id_2` int(11) NOT NULL,
+  PRIMARY KEY (`id_1`,`id_2`),
+  KEY `fk_libro_has_libro_libro2_idx` (`id_2`),
+  KEY `fk_libro_has_libro_libro1_idx` (`id_1`),
+  CONSTRAINT `fk_libro_has_libro_libro1` FOREIGN KEY (`id_1`) REFERENCES `libro` (`id`),
+  CONSTRAINT `fk_libro_has_libro_libro2` FOREIGN KEY (`id_2`) REFERENCES `libro` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequel`
+--
+
+LOCK TABLES `sequel` WRITE;
+/*!40000 ALTER TABLE `sequel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sequel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utente` (
+  `n_tessera` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` text,
+  `cognome` text,
+  `e_mail` text,
+  PRIMARY KEY (`n_tessera`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utente`
+--
+
+LOCK TABLES `utente` WRITE;
+/*!40000 ALTER TABLE `utente` DISABLE KEYS */;
+INSERT INTO `utente` VALUES (123,'$2a$04$.nMS0e9Lq9D.LYWpVhwMkuIwOAwwYPjhGWFCy4Hxmx5qZA4E.0GmS','Umberto','Cocca',NULL),(321,'$2a$04$.nMS0e9Lq9D.LYWpVhwMkuIwOAwwYPjhGWFCy4Hxmx5qZA4E.0GmS','Pino','Fumagalli',NULL);
+/*!40000 ALTER TABLE `utente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-13 15:48:36
