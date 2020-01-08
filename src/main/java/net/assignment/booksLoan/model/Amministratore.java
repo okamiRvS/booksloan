3932cb56c5b39d2a4c2ab1b732de68eaaed852b8
Silvia Traversa <s.traversa98@gmail.com>
2020-01-08 14:09:00 +0100

    Aggiornato Amministratore

--- src/main/java/net/assignment/booksLoan/model/Amministratore.java
+++ src/main/java/net/assignment/booksLoan/model/Amministratore.java
@@ -1,5 +1,18 @@
 package net.assignment.booksLoan.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Amministratore extends Utente{
+	
+	@Autowired
+	private String tipologia_contratto;
+
+	public String getTipologia_contratto() {
+		return tipologia_contratto;
+	}
+
+	public void setTipologia_contratto(String tipologia_contratto) {
+		this.tipologia_contratto = tipologia_contratto;
+	}
 	
 }
